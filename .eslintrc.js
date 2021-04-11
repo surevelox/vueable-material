9f9bbdabfec431657c77ad6b1f755546d431b22d
Niraj Soni <23270244+ns8482e@users.noreply.github.com>
2021-04-10 20:09:16 -0500

    style: Add prettier (#23)
    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
-    ecmaVersion: 2020
-  },
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+    },
 
-  root: true,
+    root: true,
 
-  env: {
-    node: true
-  },
+    env: {
+        node: true,
+    },
 
-  extends: [
-    "@vue/typescript",
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
+    extends: [
+        '@vue/typescript',
+        'plugin:vue/essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+    ],
 
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    },
 };
