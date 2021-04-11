07ef70af7cacb6614d73bbec3847bf183230da10
Niraj Soni <23270244+ns8482e@users.noreply.github.com>
2021-04-10 20:17:28 -0500

    style(v-ripple): Fix formatting

--- src/ripple/__tests__/Ripple.spec.ts
+++ src/ripple/__tests__/Ripple.spec.ts
@@ -1,140 +1,140 @@
-import { shallowMount } from "@vue/test-utils";
-import Ripple, { MDCRippleElement } from "../directive/Ripple";
-import Vue from "vue";
+import { shallowMount } from '@vue/test-utils';
+import Ripple, { MDCRippleElement } from '../directive/Ripple';
+import Vue from 'vue';
 
-describe("Directive v-ripple", () => {
-  // The component to test
-  const defaultTemplate = (caption: any) => ({
-    template: `<div v-ripple>{{ text }}</div>`,
-    props: [caption],
-    directives: {
-      Ripple
-    }
-  });
+describe('Directive v-ripple', () => {
+    // The component to test
+    const defaultTemplate = (caption: any) => ({
+        template: `<div v-ripple>{{ text }}</div>`,
+        props: [caption],
+        directives: {
+            Ripple,
+        },
+    });
 
-  const unboundedTemplate = (caption: any) => ({
-    template: `<div v-ripple.unbounded>{{ text }}</div>`,
-    props: [caption],
-    directives: {
-      Ripple
-    }
-  });
+    const unboundedTemplate = (caption: any) => ({
+        template: `<div v-ripple.unbounded>{{ text }}</div>`,
+        props: [caption],
+        directives: {
+            Ripple,
+        },
+    });
 
-  beforeAll(() => {
-    // Replace RAF with setTimeout, since setTimeout is overridden to be
-    // synchronous in Jasmine clock installation.
-    window.requestAnimationFrame = (fn: Function) => setTimeout(fn, 1);
-    window.cancelAnimationFrame = (id: number) => {
-      clearTimeout(id);
-    };
-  });
+    beforeAll(() => {
+        // Replace RAF with setTimeout, since setTimeout is overridden to be
+        // synchronous in Jasmine clock installation.
+        window.requestAnimationFrame = (fn: Function) => setTimeout(fn, 1);
+        window.cancelAnimationFrame = (id: number) => {
+            clearTimeout(id);
+        };
+    });
 
-  beforeEach(() => {
-    jest.useFakeTimers();
-  });
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
 
-  afterEach(() => {});
+    afterEach(() => {});
 
-  it("Should initialize and create mdc-component on nextTick ", done => {
-    // Prepare
+    it('Should initialize and create mdc-component on nextTick ', (done) => {
+        // Prepare
 
-    // render the component
-    const caption = "Ripple Text";
-    const wrapper = shallowMount(defaultTemplate(caption));
-    const rootEl: any = wrapper.element;
+        // render the component
+        const caption = 'Ripple Text';
+        const wrapper = shallowMount(defaultTemplate(caption));
+        const rootEl: any = wrapper.element;
 
-    let mockFn = jest.fn();
+        let mockFn = jest.fn();
 
-    // Act
-    expect(rootEl.mdcRipple).toBeUndefined();
+        // Act
+        expect(rootEl.mdcRipple).toBeUndefined();
 
-    // Assert
-    wrapper.vm
-      .$nextTick()
-      .then(() => {
-        expect(rootEl.mdcRipple).not.toBeUndefined();
-      })
-      .then(done, done);
-  });
+        // Assert
+        wrapper.vm
+            .$nextTick()
+            .then(() => {
+                expect(rootEl.mdcRipple).not.toBeUndefined();
+            })
+            .then(done, done);
+    });
 
-  it("Should initialize without unbounded modifier Falsy", done => {
-    // render the component
-    const caption = "Ripple Text";
-    const wrapper = shallowMount(defaultTemplate(caption));
+    it('Should initialize without unbounded modifier Falsy', (done) => {
+        // render the component
+        const caption = 'Ripple Text';
+        const wrapper = shallowMount(defaultTemplate(caption));
 
-    // Prepare
-    const rootEl = wrapper.element as MDCRippleElement;
+        // Prepare
+        const rootEl = wrapper.element as MDCRippleElement;
 
-    // Act
-    expect(rootEl.mdcRipple).toBeUndefined();
+        // Act
+        expect(rootEl.mdcRipple).toBeUndefined();
 
-    // Assert
-    wrapper.vm
-      .$nextTick()
-      .then(() => {
-        expect(rootEl.isUnbounded).toBeFalsy();
-      })
-      .then(done, done);
-  });
+        // Assert
+        wrapper.vm
+            .$nextTick()
+            .then(() => {
+                expect(rootEl.isUnbounded).toBeFalsy();
+            })
+            .then(done, done);
+    });
 
-  it("Should initialize with unbounded modifier Truthy", done => {
-    // render the component
-    const caption = "Ripple Text";
-    const wrapper = shallowMount(defaultTemplate(caption));
+    it('Should initialize with unbounded modifier Truthy', (done) => {
+        // render the component
+        const caption = 'Ripple Text';
+        const wrapper = shallowMount(defaultTemplate(caption));
 
-    // Prepare
-    const rootEl = wrapper.element as MDCRippleElement;
+        // Prepare
+        const rootEl = wrapper.element as MDCRippleElement;
 
-    // Act
-    expect(rootEl.mdcRipple).toBeUndefined();
+        // Act
+        expect(rootEl.mdcRipple).toBeUndefined();
 
-    // Assert
-    wrapper.vm
-      .$nextTick()
-      .then(() => {
-        expect(rootEl.isUnbounded).toBeFalsy();
-      })
-      .then(done, done);
-  });
+        // Assert
+        wrapper.vm
+            .$nextTick()
+            .then(() => {
+                expect(rootEl.isUnbounded).toBeFalsy();
+            })
+            .then(done, done);
+    });
 
-  it("handleFocus() adds class", done => {
-    // Prepare
-    // render the component
-    jest.useFakeTimers();
-    const expectedClass = "mdc-ripple-upgraded--background-focused";
-    const caption = "Ripple Text";
-    const wrapper = shallowMount(defaultTemplate(caption));
-    const rootEl = wrapper.element as MDCRippleElement;
-    wrapper.vm
-      .$nextTick()
-      .then(() => {
-        rootEl.mdcRipple?.handleFocus();
-        jest.advanceTimersByTime(1);
+    it('handleFocus() adds class', (done) => {
+        // Prepare
+        // render the component
+        jest.useFakeTimers();
+        const expectedClass = 'mdc-ripple-upgraded--background-focused';
+        const caption = 'Ripple Text';
+        const wrapper = shallowMount(defaultTemplate(caption));
+        const rootEl = wrapper.element as MDCRippleElement;
+        wrapper.vm
+            .$nextTick()
+            .then(() => {
+                rootEl.mdcRipple?.handleFocus();
+                jest.advanceTimersByTime(1);
 
-        expect(rootEl.classList).toContain(expectedClass);
-      })
-      .then(done, done);
-  });
+                expect(rootEl.classList).toContain(expectedClass);
+            })
+            .then(done, done);
+    });
 
-  it("handleBlur() removes class", done => {
-    // Prepare
-    // render the component
-    jest.useFakeTimers();
+    it('handleBlur() removes class', (done) => {
+        // Prepare
+        // render the component
+        jest.useFakeTimers();
 
-    const notExpectedClass = "mdc-ripple-upgraded--background-focused";
-    const caption = "Ripple Text";
-    const wrapper = shallowMount(defaultTemplate(caption));
-    const rootEl = wrapper.element as MDCRippleElement;
-    rootEl.classList.add(notExpectedClass);
+        const notExpectedClass = 'mdc-ripple-upgraded--background-focused';
+        const caption = 'Ripple Text';
+        const wrapper = shallowMount(defaultTemplate(caption));
+        const rootEl = wrapper.element as MDCRippleElement;
+        rootEl.classList.add(notExpectedClass);
 
-    wrapper.vm
-      .$nextTick()
-      .then(() => {
-        rootEl.mdcRipple?.handleBlur();
-        jest.advanceTimersByTime(1);
+        wrapper.vm
+            .$nextTick()
+            .then(() => {
+                rootEl.mdcRipple?.handleBlur();
+                jest.advanceTimersByTime(1);
 
-        expect(rootEl.classList).not.toContain(notExpectedClass);
-      })
-      .then(done, done);
-  });
+                expect(rootEl.classList).not.toContain(notExpectedClass);
+            })
+            .then(done, done);
+    });
 });
